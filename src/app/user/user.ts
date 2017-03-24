cb0ca937e95c3fcca1d1fb0feed2ba08219306b5
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-03-23 23:18:38 -0700

    Removed modifiers.

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,7 @@
 export class User {
-  public name: string;
-  public nameColor: string;
-  public imageUrl: string;
-  public bedId: number;
-  public bedHistory: number[];
+  name: string;
+  nameColor: string;
+  imageUrl: string;
+  bedId: number;
+  bedHistory: number[];
 }
